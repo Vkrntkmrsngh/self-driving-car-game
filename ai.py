751176376bebc1ec7dbe9ad9142d1c9ef7b34e4b
Vikrant Singh <vkrntkmrsngh@gmail.com>
2019-02-19 06:26:37 +0530

    Creation of map file
    Modification needed

---
+++ ai.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 05:51:46 2019
+
+@author: vikrant
+"""
+
+class Dqn:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
